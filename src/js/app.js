713cc48879b20617b9a83b425dcdbdb04766b5fc
ruvalter <ruvalter@gmail.com>
2016-10-13 21:01:09 -0300

    Colocamos o card como objeto

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,49 @@
-var app = angular.module("demo", []);
+var app = angular.module("demo", ['slick']);
 
 app.controller("flipperDemo", function($scope) {
-  $scope.flipped = false;
+  // $scope.flipped = false;
 
 
-  $scope.flip = function() {
-    $scope.flipped = !$scope.flipped
+  $scope.flip = function(card) {
+    card.flipped = !card.flipped;
+    // $scope.flipped = !$scope.flipped
   };
+
+  $scope.cards = [
+    {
+      title: "Run",
+      meaning: "Move fast",
+      example: "I had to run",
+      flipped: false
+    },
+    {
+      title: "Dog",
+      meaning: "It is a animal",
+      example: "Here is my new dog",
+      flipped: false
+    }
+  ];
 });
 
 app.directive("flipper", function() {
   return {
     restrict: "E",
-    template: "<div class='flipper' ng-transclude ng-class='{ flipped: flipped }'></div>",
+    template: "<div class='flipper' ng-transclude></div>",
     transclude: true,
     scope: {
-      flipped: "="
+      card: "="
+    },
+    link: function(scope, elem, attr) {
+      scope.$watch(
+        function() {
+          return scope.card.flipped;
+        },
+        function(newValue, oldValue) {
+          if (newValue !== oldValue) {
+            newValue ? elem.children().eq(0).addClass('flipped') : elem.children().eq(0).removeClass('flipped')
+          }
+        }
+      );
     }
   };
 });
